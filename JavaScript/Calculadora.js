bb62a61c6587240abaf1289cc0b2c4d25e8eba69
AnderCMD <107501858+AnderCMD@users.noreply.github.com>
2023-12-26 22:32:41 -0600

    Finalización de codigo

---
+++ JavaScript/Calculadora.js
@@ -0,0 +1,20 @@
+function Calcular() {
+    let Entrada = document.Calculadora.Resultado.value;
+    let Resultado;
+
+    if (Entrada.includes('+')) {
+        let Numeros = Entrada.split('+');
+        Resultado = parseFloat(Numeros[0]) + parseFloat(Numeros[1]);
+    } else if (Entrada.includes('-')) {
+        let Numeros = Entrada.split('-');
+        Resultado = parseFloat(Numeros[0]) - parseFloat(Numeros[1]);
+    } else if (Entrada.includes('*')) {
+        let Numeros = Entrada.split('*');
+        Resultado = parseFloat(Numeros[0]) * parseFloat(Numeros[1]);
+    } else if (Entrada.includes('/')) {
+        let Numeros = Entrada.split('/');
+        Resultado = parseFloat(Numeros[0]) / parseFloat(Numeros[1]);
+    }
+
+    document.Calculadora.Resultado.value = Resultado;
+}
